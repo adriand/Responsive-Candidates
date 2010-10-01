b81a1c092f8563e79543ff0688c5d207fef80295
Adrian Duyzer <adrian@Macbook-Pro.local>
2010-10-01 14:28:27 -0400

    the basics

--- public/application.js
+++ public/application.js
@@ -10,10 +10,16 @@ function displayResults(question_count, candidates) {
   });
   
   for (var j = 0; j < candidates.length; j++) {
+    var width = ((candidates[j].responses / question_count) * 100) + "%";
+    if (width == "0%") width = "1%"; // token line
     $("#candidates")
       .append(
-        $("<div class='candidate'>").append(
-          $("<h3>").html(candidates[j].name + " (" + candidates[j].responses + ")")
+        $("<tr>")
+        .append(
+          $("<th>").append($("<h3>").html(candidates[j].name + " (" + candidates[j].responses + ")"))
+        )
+        .append(
+          $("<td>").append($("<div class='bar'>").css("width", width))
         )
       );      
   }  
@@ -21,7 +27,7 @@ function displayResults(question_count, candidates) {
 
 $(function() {
   $('#content').corner("12px");        
-  
+  // break;
   $.getJSON("api.php?q=/election/1", function(data) {
     // get the data
     var raw_candidates = data.candidates;
